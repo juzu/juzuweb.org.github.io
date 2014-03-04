7334f7b996b6f95b1d87676e33e36db11072f903
giangnt <giangnt@exoplatform.com>
2014-03-04 17:05:31 +0700

    edit bug close dropdown-menu on mobile

--- js/bootstrap-3.1/juzu.js
+++ js/bootstrap-3.1/juzu.js
@@ -15,4 +15,10 @@
 		}
 	})
 
+	$(document).on('click.navbarCollapse', function(e) {
+		if ($(e.target).closest('.navbar-toggle').length == 0) {
+			$('#navbarCollapse').collapse('hide');
+		}		
+	});
+
 }(window.jQuery)
